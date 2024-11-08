2a832e5a1888a8e845a94e50e1638941dc13268d
Rithiarc <157029158+Rithiarc@users.noreply.github.com>
2024-11-08 14:19:24 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello World! - Rithika - 22CB045
+          Hello World! - Rithika L - 22CB045
         </p>
         <a
           className="App-link"
